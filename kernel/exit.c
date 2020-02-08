ac24c9ec5ab8b6846ad68444b7aa5d7a0373a32d
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-08 14:26:31 -0800

    simple_lmk: Place victims onto SCHED_RR
    Just increasing the victim's priority to the maximum niceness isn't
    enough to make it totally preempt everything in SCHED_FAIR, which is
    important to make sure victims die quickly. Resource-wise, this isn't
    very burdensome since the RT priority is just set to zero, and because
    dying victims don't have much to do: they only need to finish whatever
    they're doing quickly. SCHED_RR is used over SCHED_FIFO so that CPU time
    between the victims is divided evenly to help them all finish at around
    the same time, as fast as possible.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -559,9 +559,9 @@ static void exit_mm(void)
 #else
 	if (test_thread_flag(TIF_MEMDIE))
 		exit_oom_victim();
+#endif
 	if (mm_released)
 		set_tsk_thread_flag(current, TIF_MM_RELEASED);
-#endif
 }
 
 static struct task_struct *find_alive_thread(struct task_struct *p)
